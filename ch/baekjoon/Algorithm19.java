41100085ea88d0f25586813bfd50356d37381387
changheebong <model_so@naver.com>
2022-08-15 20:37:23 +0900

    2022-08-15 20:37 algorithm commit & push

---
+++ ch/baekjoon/Algorithm19.java
@@ -0,0 +1,26 @@
+package ch.baekjoon;
+
+import java.util.Scanner;
+
+/*
+백준 10870 피보나치 수 5
+ */
+public class Algorithm19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num+1];
+        for(int i = 0; i<=num; i++){
+            if( i < 2){
+                arr[i] = i;
+            }else{
+                arr[i] = arr[i-2] + arr[i-1];
+            }
+        }
+
+            System.out.println(arr[num]);
+
+
+    }
+
+}
