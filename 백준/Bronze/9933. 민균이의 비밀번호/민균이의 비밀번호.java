ac287187514caf6b5403c21db982669ba45345bf
changheeBong <66407424+changheeBong@users.noreply.github.com>
2024-07-19 15:47:33 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 152 ms, Memory: 16112 KB -BaekjoonHub

--- 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.java
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.java
@@ -1,31 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Map<String,String> map = new HashMap<>();
-
-        for(int i = 0; i< n; i ++){
-            StringBuilder sb = new StringBuilder();
-            String s = br.readLine();
-            char[] charArray = s.toCharArray();
-            for(int j = charArray.length-1 ; j > -1 ; j-- ){
-                sb.append(charArray[j]);
-            }
-            String result =  s.length() + " " + s.charAt((s.length()-1 )/2);
-            if(!map.isEmpty() && map.containsKey(s) || s.equals(sb.toString())){
-                System.out.println(result);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+        for(int i= 0; i < n ; i ++){
+            list.add(br.readLine());
+        }
+        for(int i = 0 ; i < n ; i ++){
+            StringBuffer sb = new StringBuffer(list.get(i));
+            if(list.contains(sb.reverse().toString())){
+                System.out.println(list.get(i).length() + " " + list.get(i).charAt((list.get(i).length()-1) /2));
                 break;
-            }else {
-                map.put(sb.toString(), result);
             }
         }
-    }
+
 }
+}
+
